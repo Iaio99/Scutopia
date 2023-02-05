ef5216af8f30ea334868b81383a59f959df7f7b0
Iaio99 <glnvlln0@gmail.com>
2023-02-05 13:49:23 +0100

    Refactoring

--- Scutopia/urls.py
+++ Scutopia/urls.py
@@ -1,6 +1,8 @@
 import django.contrib.auth.urls
 from django.contrib.auth import views as auth_views
 from django.urls import re_path, include, path
+from django.views.generic.base import RedirectView
+
 from . import views
 
 urlpatterns = [
@@ -10,5 +12,5 @@ urlpatterns = [
     re_path(r'^ssd/', views.view_authors),
     re_path(r'^adu/', views.view_adu),
     re_path(r'^accounts/', include("django.contrib.auth.urls")),
-    re_path(r'^accounts/login/', auth_views.LoginView.as_view())
+#    re_path(r'^login', r'^/accounts/login')
 ]
