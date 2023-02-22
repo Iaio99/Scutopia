23cc1213c813d46409493a300ffd5693aea17331
andrea <andreacantarini2015@gmail.com>
2023-02-22 02:21:41 +0100

    ass

--- frontend/src/app/component/publication/publication.component.ts
+++ frontend/src/app/component/publication/publication.component.ts
@@ -30,9 +30,9 @@ export class PublicationComponent implements OnInit {
                 eid__magazine: publication[4],
                 eid__volume: publication[5],
                 eid__page_range: publication[6],
-                eid__doi: publication[7],
+                eid__doi: "https://doi.org/"+publication[7],
                 eid__download_date: publication[8],
-                scopus_id__ssd: "https://doi.org/"+publication[9]
+                scopus_id__ssd: publication[9]
               } as Publication
             );
             console.log(this.publications);
